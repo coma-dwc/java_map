4bee65b1c8f6818439401a68c48b3c132100a9c2
coma-dwc <tamagomapon@gmail.com>
2023-08-20 16:34:58 +0900

    first commit "HashMapの作成"

---
+++ src/map_make/Main.java
@@ -0,0 +1,11 @@
+package map_make;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		System.out.println(map);
+	}
+}
