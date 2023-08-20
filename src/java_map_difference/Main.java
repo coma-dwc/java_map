753043488cf2b1a3f865487d8b3d50e9846e81d9
coma-dwc <tamagomapon@gmail.com>
2023-08-20 22:56:22 +0900

    独自クラスをキーに使う

--- src/java_map_difference/Main.java
+++ src/java_map_difference/Main.java
@@ -5,28 +5,76 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+//public class Main {
+//	private static void testMap(Map<String, String> map) {
+//		map.put("alpha", "α");
+//		map.put("beta", "β");
+//		map.put("gamma", "γ");
+//		map.put("delta", "δ");
+//		map.put("epsilon", "ε");
+//		map.put("zeta", "ζ");
+//		map.put("eta", "η");
+//		map.put("theta", "θ");
+//		
+//		System.out.println(map.getClass());
+//		map.forEach((key, value) -> {
+//			System.out.println(key + " = " + value);
+//		});
+//	}
+//	public static void main(String[] args) {
+//		testMap(new HashMap<String, String>());
+//		testMap(new TreeMap<String, String>());
+//		testMap(new LinkedHashMap<String, String>());
+//	}
+//	//HashMapではキーの並び順は不定
+//	//TreeMapではキーの並び順は辞書順
+//	//LinkedHashMapではキーの並び順は追加順
+//}
+
+//独自クラスをキーに使う
+//Stringをラップする独自クラスMyKeyを定義してそれをMapのキーとして使用
+class MyKey {
+	
+	private String key;
+	
+	public MyKey(String key) {
+		this.key = key;
+	}
+	
+	public String getKey() {
+		return this.key;
+	}
+	
+	@Override
+	public String toString() {
+		return this.key;
+	}
+}
+
 public class Main {
-	private static void testMap(Map<String, String> map) {
-		map.put("alpha", "α");
-		map.put("beta", "β");
-		map.put("gamma", "γ");
-		map.put("delta", "δ");
-		map.put("epsilon", "ε");
-		map.put("zeta", "ζ");
-		map.put("eta", "η");
-		map.put("theta", "θ");
+	private static void testMap(Map<MyKey, String> map) {
+		map.put(new MyKey("alpha"), "α");
+		map.put(new MyKey("beta"), "β");
+		map.put(new MyKey("gamma"), "γ");
+		map.put(new MyKey("delta"), "δ");
+		map.put(new MyKey("epsilon"), "ε");
+		map.put(new MyKey("zeta"), "ζ");
+		map.put(new MyKey("eta"), "η");
+		map.put(new MyKey("theta"), "θ");
 		
 		System.out.println(map.getClass());
-		map.forEach((key, value) -> {
-			System.out.println(key + " = " + value);
-		});
+		for(MyKey key : map.keySet()) {
+			MyKey alternativeKey = new MyKey(key.getKey());
+			System.out.println(key + " = " + map.get(key) + " = " + map.get(alternativeKey));
+		}
 	}
 	public static void main(String[] args) {
-		testMap(new HashMap<String, String>());
-		testMap(new TreeMap<String, String>());
-		testMap(new LinkedHashMap<String, String>());
+		testMap(new HashMap<MyKey, String>());
+		testMap(new LinkedHashMap<MyKey, String>());
+		testMap(new TreeMap<MyKey, String>());
 	}
-	//HashMapではキーの並び順は不定
-	//TreeMapではキーの並び順は辞書順
-	//LinkedHashMapではキーの並び順は追加順
+	
+	//HashMapとLinkedHashMapで、同じMyKeyのインスタンスを使用している場合は、getで値が取得できているが、
+	//同じ値を持つMyKeyを新たに生成した場合は返り値がnullになっている
+	//TreeMapではmap.putのところで実行時エラーになる(MyKeyクラスがMapのキーとして備えるべきメソッドを定義していないため)
 }
