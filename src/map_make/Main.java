99ebfadee7e971079edaa6e83f741885c94dd13e
coma-dwc <tamagomapon@gmail.com>
2023-08-20 16:42:21 +0900

    TreeMapの作成

--- src/map_make/Main.java
+++ src/map_make/Main.java
@@ -1,11 +1,35 @@
 package map_make;
 
-import java.util.HashMap;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Main {
+//	public static void main(String[] args) {
+//		Map<String, String> map = new HashMap<>();
+//		System.out.println(map);
+//	}
+//}
+
+
+//TreeMapの作成;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<>();
+		Map<String, String> map = new TreeMap<>();
 		System.out.println(map);
 	}
 }
+
+
+//LinkedHashMapの作成
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//public class Main {
+//	public static void main(String[] args) {
+//		Map<String, String> map = new LinkedHashMap<>();
+//		System.out.println(map);
+//	}
+//}
